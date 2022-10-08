8fff6f6d1c50cd910ecb8583823ac50683ad3762
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-10-08 10:07:43 -0400

    Added all left associative infix operators

--- lib/parse.ml
+++ lib/parse.ml
@@ -30,42 +30,60 @@ let print_ast =
             helper (indentation + 1) b;
     in helper 0;;
 
-let parse_value l =
-    match Lexer.lex l with
-    | Ok { filename; line; col; token = Lexer.Float f } -> Ok { filename; line; col; ast = Float f }
-    | Ok _                                              -> Error "oh no"
-    | Error e                                           -> Error e;;
-
-let parse_mult l =
+let parse_linfix ops next l =
     let rec helper a l =
         let state = Lexer.push_lexer l in
             match Lexer.lex l with
-            | Ok { filename; line; col; token = Lexer.Star | Lexer.Slash | Lexer.Percent as token } ->
-                begin
-                    match parse_value l with
+            | Ok { filename; line; col; token } ->
+                if List.mem token ops then
+                    match next l with
                     | Ok v ->
                         begin
                             let op = match token with
-                                     | Lexer.Star    -> Mul
-                                     | Lexer.Slash   -> Div
-                                     | Lexer.Percent -> Mod
+                                     | Lexer.Star        -> Mul
+                                     | Lexer.Slash       -> Div
+                                     | Lexer.Percent     -> Mod
+                                     | Lexer.Plus        -> Add
+                                     | Lexer.Minus       -> Sub
+                                     | Lexer.DoubleColon -> Cons
                                      | _ -> raise Exit
                             in helper { filename; line; col; ast = BinOp (op, a, v) } l
                         end
                     | Error e ->
                         Lexer.pop_lexer l state;
                         Error e
+                else begin
+                    Lexer.pop_lexer l state;
+                    Ok a
                 end
-            | Ok _    ->
-                Lexer.pop_lexer l state;
-                Ok a
             | Error e ->
                 Lexer.pop_lexer l state;
                 Error e
-    in match parse_value l with
+    in match next l with
     | Ok v -> helper v l
     | Error e -> Error e;;
 
+let rec parse_value l =
+    match Lexer.lex l with
+    | Ok { filename; line; col; token = Lexer.Float f } -> Ok { filename; line; col; ast = Float f }
+    | Ok { token = Lexer.LParen; _ }                    ->
+        begin
+            match parse_add l with
+            | Ok v    ->
+                begin
+                    match Lexer.lex l with
+                    | Ok { token = Lexer.RParen; _ } -> Ok v
+                    | Ok _                           -> Error "oh no"
+                    | Error e                        -> Error e
+                end
+            | Error e -> Error e
+        end
+    | Ok _                                              -> Error "oh no"
+    | Error e                                           -> Error e
+
+and parse_mult l = parse_linfix [Lexer.Star; Lexer.Slash; Lexer.Percent] parse_value l
+and parse_add l = parse_linfix [Lexer.Plus; Lexer.Minus] parse_mult l;;
+
 let parse filename contents =
     let l = Lexer.create_lexer filename contents
-    in parse_mult l
+    in parse_add l;;
