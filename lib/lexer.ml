7473efea8a0bce59af2adda530c75d714d70476e
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-10-08 08:58:10 -0400

    Added multiline comments

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -13,7 +13,7 @@ type lexer = {
 
 let create_lexer filename contents = { contents; filename; index = 0; line = 1; col = 0 };;
 
-type state = Start | FloatTop | FloatPastDot | FloatPastEe | FloatPastEeFirst;;
+type state = Start | MultilineComment of int | FloatTop | FloatPastDot | FloatPastEe | FloatPastEeFirst;;
 
 let lex l =
     let rec helper s state line col l =
@@ -27,6 +27,11 @@ let lex l =
                 begin
                     match c with
                     | '0' .. '9'        -> helper (s ^ String.make 1 c) FloatTop line col l
+                    | '('               ->
+                        if String.length l.contents > l.index && l.contents.[l.index] == '*' then
+                            let _ = l.index <- l.index + 1 in
+                            helper s (MultilineComment 1) line col l
+                        else Error "invalid character"
                     | ' ' | '\t' | '\r' -> helper s Start l.line l.col l
                     | '\n'              ->
                         l.line <- l.line + 1;
@@ -37,20 +42,47 @@ let lex l =
                             Ok { filename = l.filename; line; col; token = Eof }
                         else Error "invalid character"
                 end
+            | MultilineComment level ->
+                begin
+                    match c with
+                    | '(' ->
+                        if String.length l.contents > l.index && l.contents.[l.index] == '*' then
+                            let _ = l.index <- l.index + 1 in
+                            helper s (MultilineComment (level + 1)) line col l
+                        else helper s (MultilineComment level) line col l
+                    | '*' ->
+                        if String.length l.contents > l.index && l.contents.[l.index] == ')' then
+                            let _ = l.index <- l.index + 1 in
+                            if level == 1 then
+                                helper s Start l.line l.col l
+                            else helper s (MultilineComment (level - 1)) line col l
+                        else helper s (MultilineComment level) line col l
+                    | '\n' ->
+                        l.line <- l.line + 1;
+                        l.col <- 0;
+                        helper s (MultilineComment level) line col l
+                    | _    -> helper s (MultilineComment level) line col l
+                end
             | FloatTop ->
                 begin
                     match c with
                     | '0' .. '9' -> helper (s ^ String.make 1 c) FloatTop line col l
                     | '.'        -> helper (s ^ String.make 1 c) FloatPastDot line col l
                     | 'e' | 'E'  -> helper (s ^ String.make 1 c) FloatPastEe line col l
-                    | _          -> Ok { filename = l.filename; line; col; token = Float (float_of_string s) }
+                    | _          ->
+                        l.index <- l.index - 1;
+                        l.col <- l.col - 1;
+                        Ok { filename = l.filename; line; col; token = Float (float_of_string s) }
                 end
             | FloatPastDot ->
                 begin
                     match c with
                     | '0' .. '9' -> helper (s ^ String.make 1 c) FloatPastDot line col l
                     | 'e' | 'E'  -> helper (s ^ String.make 1 c) FloatPastEe line col l
-                    | _          -> Ok { filename = l.filename; line; col; token = Float (float_of_string s) }
+                    | _          ->
+                        l.index <- l.index - 1;
+                        l.col <- l.col - 1;
+                        Ok { filename = l.filename; line; col; token = Float (float_of_string s) }
                 end
             | FloatPastEe ->
                 begin
@@ -62,7 +94,10 @@ let lex l =
                 begin
                     match c with
                     | '0' .. '9' -> helper (s ^ String.make 1 c) FloatPastEeFirst line col l
-                    | _          -> Ok { filename = l.filename; line; col; token = Float (float_of_string s) }
+                    | _          ->
+                        l.index <- l.index - 1;
+                        l.col <- l.col - 1;
+                        Ok { filename = l.filename; line; col; token = Float (float_of_string s) }
                 end
     in helper "" Start l.line l.col l;;
 
